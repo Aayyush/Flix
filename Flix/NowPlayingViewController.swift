65de8bf5f02203cfda6af44532990ddf636dba29
Aayush Gupta <aayush@Aayushs-Mac.local>
2018-02-04 23:51:19 -0500

    Inital Commit. Project Complete.

---
+++ Flix/NowPlayingViewController.swift
@@ -0,0 +1,48 @@
+//
+//  NowPlayingViewController.swift
+//  Flix
+//
+//  Created by Aayush Gupta on 2/4/18.
+//  Copyright © 2018 Aayush Gupta. All rights reserved.
+//
+
+import UIKit
+
+class NowPlayingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data{
+                let dataDict = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+                print(dataDict)
+            }
+        }
+        task.resume()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
